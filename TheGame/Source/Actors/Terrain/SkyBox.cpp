830a2c81161b2103886828698e58f0def95feb57
David Siekut <davidsiekut@gmail.com>
2014-08-12 20:53:07 -0400

    apparently a depth of 0.1 is bad

--- TheGame/Source/Actors/Terrain/SkyBox.cpp
+++ TheGame/Source/Actors/Terrain/SkyBox.cpp
@@ -3,10 +3,10 @@
 const float SkyBox::SKYBOX_X = 1024.f;
 const float SkyBox::SKYBOX_Y = 512.f;
 
-SkyBox::SkyBox(Entity *parent, Arwing *arwing) : Cube(parent, glm::vec3(SKYBOX_X, SKYBOX_Y, 0.1f), glm::vec2(SKYBOX_X / 1024, SKYBOX_Y / 512))
+SkyBox::SkyBox(Entity *parent, Arwing *arwing) : Cube(parent, glm::vec3(SKYBOX_X, SKYBOX_Y, 0.1f), glm::vec2(SKYBOX_X / 1024.f, SKYBOX_Y / 512.f))
 {
 	name = "SKYBOX";
-	size = glm::vec3(SKYBOX_X, SKYBOX_Y, 0.1f);
+	size = glm::vec3(SKYBOX_X, SKYBOX_Y, 1.f);
 	this->arwing = arwing;
 	//this->SetPosition(glm::vec3(parent->GetPosition().x, parent->GetPosition().y, parent->GetPosition().z + 100));
 	objPath = "../Assets/Models/cube.obj";
