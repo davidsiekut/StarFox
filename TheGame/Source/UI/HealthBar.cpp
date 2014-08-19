7ba43979f9b3f77da7ccfa824312a4fc5041b22b
David Siekut <davidsiekut@gmail.com>
2014-08-19 18:41:14 -0400

    templates

--- TheGame/Source/UI/HealthBar.cpp
+++ TheGame/Source/UI/HealthBar.cpp
@@ -1,3 +1,18 @@
+//
+// COMP 371 Term Project
+//
+// Created by
+// Boutas, Vasiliki   (6220304)
+// Di Girolamo, John  (6202918)
+// Ozgaon, Dror Asher (6296742)
+// Siekut, David      (6329810)
+// Tran, Quang        (6339816)
+// Wan, Kwok - Chak   (6291643)
+//
+// Contributions to this file:
+//
+//
+
 #include "HealthBar.h"
 #include "Scene.h"
 #include "UltraBoss.h"
--- TheGame/Source/UI/UIElement.cpp
+++ TheGame/Source/UI/UIElement.cpp
@@ -1,3 +1,18 @@
+//
+// COMP 371 Term Project
+//
+// Created by
+// Boutas, Vasiliki   (6220304)
+// Di Girolamo, John  (6202918)
+// Ozgaon, Dror Asher (6296742)
+// Siekut, David      (6329810)
+// Tran, Quang        (6339816)
+// Wan, Kwok - Chak   (6291643)
+//
+// Contributions to this file:
+//
+//
+
 #include "UIElement.h"
 #define GLM_FORCE_RADIANS
 #define dtor(x) x*(3.141592f/180.0f)
